b519680b2e1a209a4ab4f2f4687e79bf7f05f69f
Janakrishnamoorthy2003 <145537564+Janakrishnamoorthy2003@users.noreply.github.com>
2023-09-20 12:45:42 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,27 @@
+
+$('.owl-car').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+
+    responsive: {
+        0: {
+            items: 1,
+
+        },
+        600: {
+            items: 2,
+
+        },
+        1000: {
+            items: 4,
+
+        }
+    }
+})
+
+
+
+
---
+++ netflix.js
@@ -0,0 +1,49 @@
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+
+    responsive: {
+        0: {
+            items: 1,
+
+        },
+        600: {
+            items: 2,
+
+        },
+        1000: {
+            items: 4,
+
+        }
+    }
+})
+
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: false,
+//     autoplay: true,
+//     autoplayTimeout: 2000,
+
+//     responsive: {
+//         0: {
+//             items: 1,
+
+//         },
+//         600: {
+//             items: 2,
+
+//         },
+//         1000: {
+//             items: 4,
+
+//         }
+//     }
+// })
+
+
+
